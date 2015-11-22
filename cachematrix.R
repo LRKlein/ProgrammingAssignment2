135a13be5ec34bf4bb86ca1b04f28d0f8054e441
L_Klein <l.klein11@gmx.de>
2015-11-22 15:16:12 +0100

    Programming Assignment 2 completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Note: My function stores the matrix under the name "mat1" instead of "x". 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat1=matrix()) { ## With the input you give makeCacheMatrix creates a matrix
+    m <- NULL                                ## and you store it under a variable, e.g. like test<-makeCacheMatrix(matrix(...))
+    set <- function(y) {                     
+        mat1 <<- y                           ## The rest what this function does, is to store 4 functions as a list,
+        m <<- NULL                           ## namely set, get, setinverse and getinverse. No calculation is performed.
+    }
+    get <- function() mat1
+    setinverse <- function(inverse) m <<- inverse 
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the matrix created with makeCacheMatrix. 
+## Furthermore, it uses the functions that are stored in makeCacheMatrix.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mat1,...) {           ## You need to run this function on the variable you named before, e.g. test (see above)
+    m <- mat1$getinverse()                   ## Here it gets the inverse of the matrix that is calculated in this function.
+    if(!is.null(m)) {                        ## If there is an inverse because it has been calculated already, this inverse
+        message("getting cached data")       ## simply gets retrieved and it says "getting cached data" and there is no need to calculate it again.
+        return(m)
+    }
+    data <- mat1$get()                      
+    m <- solve(data, ...)                    ## If the inverse has not yet been calculated, here it gets calculated (with solve()).
+    mat1$setinverse(m)
+    m                                        ## Return a matrix that is the inverse of 'mat1'
 }
+
